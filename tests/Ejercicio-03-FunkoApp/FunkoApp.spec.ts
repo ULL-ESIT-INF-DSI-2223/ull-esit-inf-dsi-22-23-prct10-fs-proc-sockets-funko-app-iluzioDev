67d52b2a7b72d4c25973eebfa0580f91126d9b9c
IluzioDev <luischinearangel@gmail.com>
2023-04-15 03:11:27 +0100

    Updating

--- tests/Ejercicio-03-FunkoApp/FunkoApp.spec.ts
+++ tests/Ejercicio-03-FunkoApp/FunkoApp.spec.ts
@@ -7,39 +7,40 @@ import {
 } from '../../src/Ejercicio-03-FunkoApp/FunkoApp/FunkoApp.js'
 import { Client } from '../../src/Ejercicio-03-FunkoApp/FunkoApp/Client.js'
 
-const funkoApp: FunkoApp = new FunkoApp(3001)
-const client: Client = new Client(3001)
+describe('Funko App Tests', () => {
+  const funkoApp: FunkoApp = new FunkoApp(3000)
+  const client: Client = new Client(3000)
+  describe('FunkoApp class tests', () => {
+    it('FunkoApp should be an instance of FunkoApp class', () => {
+      expect(funkoApp).to.be.an.instanceof(FunkoApp)
+    })
+    it('FunkoApp should have a port property', () => {
+      expect(funkoApp).to.have.property('port')
+      expect(funkoApp.port).to.be.a('number')
+    })
+    it('FunkoApp should have a server property', () => {
+      expect(funkoApp).to.have.property('server')
+    })
+  })
 
-describe('FunkoApp class tests', () => {
-  it('FunkoApp should be an instance of FunkoApp class', () => {
-    expect(funkoApp).to.be.an.instanceof(FunkoApp)
-  })
-  it('FunkoApp should have a port property', () => {
-    expect(funkoApp).to.have.property('port')
-    expect(funkoApp.port).to.be.a('number')
-  })
-  it('FunkoApp should have a server property', () => {
-    expect(funkoApp).to.have.property('server')
-  })
-})
-
-describe('Client class tests', () => {
-  it('Client should be an instance of Client class', () => {
-    expect(client).to.be.an.instanceof(Client)
-  })
-  it('Client should have a socket property', () => {
-    expect(client).to.have.property('socket')
-  })
-  it('Client should have a port property', () => {
-    expect(client).to.have.property('port')
-    expect(client.port).to.be.a('number')
-  })
-  it('Client should be able to connect to a server, send a command and receive the output', () => {
-    const request: RequestType = {
-      user: 'Iluzio',
-      type: 'list',
-    }
-    expect(client.connect(request)).to.be.undefined
-    funkoApp.server.close()
+  describe('Client class tests', () => {
+    it('Client should be an instance of Client class', () => {
+      expect(client).to.be.an.instanceof(Client)
+    })
+    it('Client should have a socket property', () => {
+      expect(client).to.have.property('socket')
+    })
+    it('Client should have a port property', () => {
+      expect(client).to.have.property('port')
+      expect(client.port).to.be.a('number')
+    })
+    it('Client should be able to connect to a server, send a command and receive the output', () => {
+      const request: RequestType = {
+        user: 'Iluzio',
+        type: 'list',
+      }
+      expect(client.connect(request)).to.be.undefined
+      funkoApp.server.close()
+    })
   })
 })
--- tests/PE102/test.spec.ts
+++ tests/PE102/test.spec.ts
@@ -7,35 +7,37 @@ import { Server } from '../../src/PE102/Server.js'
 const web_server: Server = new Server(3000)
 const client: Client = new Client(3000)
 
-describe('Server class tests', () => {
-  it('Web Server should be an instance of Server class', () => {
-    expect(web_server).to.be.an.instanceof(Server)
-  })
-  it('Web Server should have a port property', () => {
-    expect(web_server).to.have.property('port')
-    expect(web_server.port).to.be.a('number')
-  })
-  it('Web Server should have a server property', () => {
-    expect(web_server).to.have.property('server')
-  })
-  it('Web Server should be able to listen to a port and handle connections', () => {
-    web_server.listen((success) => {
-      expect(success).to.be.true
+describe('PE102 tests', () => {
+  describe('Server class tests', () => {
+    it('Web Server should be an instance of Server class', () => {
+      expect(web_server).to.be.an.instanceof(Server)
+    })
+    it('Web Server should have a port property', () => {
+      expect(web_server).to.have.property('port')
+      expect(web_server.port).to.be.a('number')
+    })
+    it('Web Server should have a server property', () => {
+      expect(web_server).to.have.property('server')
+    })
+    it('Web Server should be able to listen to a port and handle connections', () => {
+      web_server.listen((success) => {
+        expect(success).to.be.true
+      })
     })
   })
-})
 
-describe('Client class tests', () => {
-  it('Client should be an instance of Client class', () => {
-    expect(client).to.be.an.instanceof(Client)
-  })
-  it('Client should have a socket property', () => {
-    expect(client).to.have.property('socket')
-  })
-  it('Client should be able to connect to a server, send a command and receive the output', () => {
-    client.connect(['node', 'test.spec.ts', 'ls', '-l'], (success) => {
-      expect(success).to.be.true
-      web_server.server.close()
+  describe('Client class tests', () => {
+    it('Client should be an instance of Client class', () => {
+      expect(client).to.be.an.instanceof(Client)
+    })
+    it('Client should have a socket property', () => {
+      expect(client).to.have.property('socket')
+    })
+    it('Client should be able to connect to a server, send a command and receive the output', () => {
+      client.connect(['node', 'test.spec.ts', 'ls', '-l'], (success) => {
+        expect(success).to.be.true
+        web_server.server.close()
+      })
     })
   })
 })
